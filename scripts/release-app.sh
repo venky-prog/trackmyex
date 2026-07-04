cf71a31ce6a6f87b86e61601a7f526f84a20accb
venkatesh <venkatesh.morla@hotmail.com>
2026-07-04 16:25:20 +0530

    actions

---
+++ scripts/release-app.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -euo pipefail
+
+mkdir -p ~/.ssh
+echo "$SSH_KEY" > ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa
+ssh-keyscan -H "$SERVER_HOST" >> ~/.ssh/known_hosts 2>/dev/null
+
+echo "Deploying to nginx"
+
+pnpm codegen
+
+pnpm build
+
+
+rsync -avz \
+      -e "ssh -i ~/.ssh/id_rsa" \
+      "./dist/" \
+      "$SERVER_USER@$SERVER_HOST:$APPS_PATH/"
